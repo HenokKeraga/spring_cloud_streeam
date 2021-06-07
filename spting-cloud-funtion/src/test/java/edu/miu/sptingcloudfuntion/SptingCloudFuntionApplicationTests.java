f716ee8ed16b3e22812783ef19c7d5191b71ef41
henok keraga <henokkeraga@gmail.com>
2021-06-06 21:50:42 -0600

    test

--- spting-cloud-funtion/src/test/java/edu/miu/sptingcloudfuntion/SptingCloudFuntionApplicationTests.java
+++ spting-cloud-funtion/src/test/java/edu/miu/sptingcloudfuntion/SptingCloudFuntionApplicationTests.java
@@ -1,13 +1,30 @@
 package edu.miu.sptingcloudfuntion;
 
+import edu.miu.sptingcloudfunction.business.TodoBusinessImpl;
+import edu.miu.sptingcloudfunction.business.api.TodoService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SptingCloudFuntionApplicationTests {
+    @Mock
+    TodoService todoService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
 
     @Test
     void contextLoads() {
+        var todoBusiness = new TodoBusinessImpl(todoService);
+        int result = todoBusiness.toTest();
+        Assertions.assertEquals(4,result);
     }
+
 
 }
